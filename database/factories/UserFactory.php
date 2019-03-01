a1b6ccdb3cde55046ed74cdd3b6c952368f3270e
lgs1226222530 <122222530@qq.com>
2019-03-02 00:48:27 +0800

    某个用户的话题列表

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,20 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
     static $password;
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    // 传参为生成最大时间不超过，创建时间永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
+    $introduction ="这个人很懒，什么都没留下！";
 
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('123123'),
         'remember_token' => str_random(10),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
+        'introduction' => $introduction,
     ];
 });
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -20,6 +20,8 @@
                         <hr>
                         <h4><strong>注册于</strong></h4>
                         <p>{{ $user->created_at->diffForHumans() }}</p>
+                        <h4><strong>活跃于</strong></h4>
+                        <p>{{ $user->updated_at->diffForHumans() }}</p>
                     </div>
                 </div>
             </div>
@@ -38,10 +40,13 @@
         {{-- 用户发布的内容 --}}
         <div class="panel panel-default">
             <div class="panel-body">
-                暂无数据 ~_~
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#">Ta 的话题</a></li>
+                    <li><a href="#">Ta 的回复</a></li>
+                </ul>
+                @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
             </div>
         </div>
-
     </div>
 </div>
 @stop
