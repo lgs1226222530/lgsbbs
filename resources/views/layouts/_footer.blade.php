8bc8fe9f77d4cd4851042300d1544529ad9c061e
lgs1226222530 <122222530@qq.com>
2019-03-01 15:35:52 +0800

    创建基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://sighttp.qq.com/msgrd?v=1&uin=1226222530" target="_blank">中二少年ID</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:1226222530@qq.com">邮箱联系</a></p>
+    </div>
+</footer>
